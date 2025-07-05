ed5b2e9817037eb61f1d6d2be513f830459be920
HerlynMae <torresherlynmae@gmail.com>
2025-07-05 19:57:26 +0800

    first commit

---
+++ dist/js/slider.js
@@ -0,0 +1,14 @@
+var slider = tns({
+  container: ".testimonialsSlider",
+  items: 1,
+  slideBy: 1,
+  speed: 200,
+  autoplay: true,
+  nav: true,
+  autoplayButtonOutput: false,
+  rewind: true,
+  navPosition: "bottom",
+  containerControls: "#controls",
+  prevButton: ".prev",
+  nextButton: ".next",
+});
